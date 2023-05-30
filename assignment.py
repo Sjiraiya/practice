093e4ef744778040e16e29a08eab925cceeee0bc
hari <b2.sachit@cgim.edu.np>
2023-05-30 21:08:45 +0545

    now commiting for assignment

---
+++ assignment.py
@@ -0,0 +1,28 @@
+# name = "shashank"
+# color ="pink"
+# age= 16
+# print("hi {}! your favourite color is {} and your age is {}".format(name,color,age))
+# print(f"hi {name}! your favourite color is {color}. ok? and you are in you {age}s")
+
+# # program that counts words in a sentence
+# sentence = input("enter a sentence please: ")
+# list = sentence.split(" ")
+# count =0
+# for x in list:
+#     count=count+1
+# print (count)
+# # also
+# print (len(list))
+
+# names = input("enter your full name: ")
+# # names = "hari sharma"
+# name = names.upper()
+# chuttau = name.split(" ")
+# # chuttau[0]=chuttau[1]
+# print(chuttau[1],",", chuttau[0])
+
+# sent= input("enter a sentence: ")
+# prev = input("enter the word to be replaced: ")
+# next = input("enter the word that replaces: ")
+# newsent = sent.replace(prev, next)
+# print(newsent)
